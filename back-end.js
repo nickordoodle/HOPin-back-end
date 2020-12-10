897121bcc63a81932be9135b3f4a7d3a7bb69af4
nscherer30 <nscherer30@gmail.com>
2020-12-10 13:27:30 -0800

    test post route

--- back-end.js
+++ back-end.js
@@ -10,7 +10,13 @@ server.listen(process.env.PORT || 3000);
 
 var userFavorites = ["blue moon", "budweiser"];
 
-//route to return list of all employees
-server.get("/favorites", (req, res) => {
+//route to get all user beer favorites
+server.get("/user/favorites", (req, res) => {
     res.send(userFavorites);
+});
+
+//route to add new beer favorites for the user
+server.post("/user/favorites", (req, res) => {
+    userFavorites.push(req.body);
+    res.send("success");
 });
