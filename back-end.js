eb41aa7f12d9c80d9def443fa7b61c39d6ec14e5
nscherer30 <nscherer30@gmail.com>
2020-12-15 10:52:04 -0800

    func change

--- back-end.js
+++ back-end.js
@@ -9,21 +9,6 @@ const client = new MongoClient(uri, {
     useNewUrlParser: true
 });
 
-function isDuplicate(beerId, beerName) {
-    userFavorites.map(beer => {
-        console.log(Number(beer.id));
-        console.log(Number(beerId));
-        console.log(beer.name.toLowerCase());
-        console.log(beerName.toLowerCase());
-        if (Number(beer.id) === Number(beerId) ||
-            beer.name.toLowerCase() === beerName.toLowerCase()) {
-            console.log("isDuplicate returns true");
-            return true;
-        }
-    });
-    return false;
-}
-
 client.connect(err => {
     const collection = client.db("users").collection("favorites");
     console.log("Mongo connection opened.");
@@ -95,9 +80,8 @@ server.get("/user/favorites", (req, res) => {
 server.post("/user/favorites", (req, res) => {
     //TODO Check if beer is already in favorites
     let newId = req.body.id;
-    let newName = req.body.name;
-    var isDuplicate = isDuplicate(newId, newName);
-    if (isDuplicate) {
+    let newName = req.body.name;;
+    if (isDuplicate(newId, newName) === true) {
         sendFailed404Error(res, "Oops, it looks like you already have that beer favorited.");
     } else {
         userFavorites.push(req.body);
@@ -183,4 +167,19 @@ function sendSuccessResponse(res) {
 
 function sendFailed404Error(res, msg) {
     res.status(404).send(msg);
+}
+
+function isDuplicate(beerId, beerName) {
+    userFavorites.map(beer => {
+        console.log(Number(beer.id));
+        console.log(Number(beerId));
+        console.log(beer.name.toLowerCase());
+        console.log(beerName.toLowerCase());
+        if (Number(beer.id) === Number(beerId) ||
+            beer.name.toLowerCase() === beerName.toLowerCase()) {
+            console.log("isDuplicate returns true");
+            return true;
+        }
+    });
+    return false;
 }
